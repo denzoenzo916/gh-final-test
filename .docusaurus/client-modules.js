b7723b30d88e8ee1fe6efeb9683479c426dccb2e
denzoenzo916 <hoang.denny@gmail.com>
2025-05-10 21:36:09 -0700

    remove files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/DenzoEnzo/Desktop/Git/gh-final-test/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/DenzoEnzo/Desktop/Git/gh-final-test/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/DenzoEnzo/Desktop/Git/gh-final-test/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/DenzoEnzo/Desktop/Git/gh-final-test/src/css/custom.css"),
-];
